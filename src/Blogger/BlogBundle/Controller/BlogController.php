03c74fc1ca63d8c136676f6e8fcb0e920c83b6b1
jesusrangel2 <leon_r2@hotmail.com>
2012-08-19 17:52:27 -0430

    tutrial completo hasta la parte 3, comenzando la parte 4

--- src/Blogger/BlogBundle/Controller/BlogController.php
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -5,27 +5,26 @@ namespace Blogger\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
- * Controlador del Bolg
+ * Controlador del Blog.
  */
+class BlogController extends Controller
+{
+    /**
+     * Muestra una entrada del blog
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
 
-class BlogController extends Controller{
-	
-	/**
-	 * Muestra una entrada del blog
-	 */
-	 public function showAction($id){
-	 	
-		$em = $this->getDoctrine()->getEntityManager();
-		
-		$blog = $em->getRepository('BloggerBlogBundle')->find($id);
-		
-		if (!$blog) {
-			throw $this->createNotFoundException('No se encontro el articulo que busca');
-		}
-		
-		return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
-			'blog'=>$blog
-		));
-	 }
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+        ));
+    }
 }
 ?>
